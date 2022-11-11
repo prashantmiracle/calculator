470bb36cfbccb5981b055bd9dd5e99bf6cd5cef1
prashant <prashantmiracle@gmail.com>
2022-11-11 08:12:01 +0530

    2

--- calculator.py
+++ calculator.py
@@ -5,9 +5,34 @@ import parser
 root = Tk()
 root.title('calculator')
 
-#Adding input field
-display=Entry(root)
-display.grid(row=1,columnspan=6,sticky=W+E)
+# Adding input field
+display = Entry(root)
+display.grid(row=1, columnspan=6, sticky=W+E)
+
+# adding buttons to the calculator
+
+Button(root, text="1").grid(row=2, column=0)
+Button(root, text="2").grid(row=2, column=1)
+Button(root, text="3").grid(row=2, column=2)
+
+Button(root, text="4").grid(row=3, column=0)
+Button(root, text="5").grid(row=3, column=1)
+Button(root, text="6").grid(row=3, column=2)
+
+
+Button(root, text="7").grid(row=4, column=0)
+Button(root, text="8").grid(row=4, column=1)
+Button(root, text="9").grid(row=4, column=2)
+
+# Adding other buttons in calculator
+Button(root, text="AC").grid(row=5, column=0)
+Button(root, text="0").grid(row=5, column=1)
+Button(root, text="=C").grid(row=5, column=2)
+
+Button(root, text="+").grid(row=2, column=3)
+Button(root, text="-").grid(row=3, column=3)
+Button(root, text="*").grid(row=4, column=3)
+Button(root, text="/").grid(row=5, column=3)
 
 
 root.mainloop()
